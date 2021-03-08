db583f85af06be3d3597cbb0a277a7f397c78382
Seyoun Lee <54106189+youn16@users.noreply.github.com>
2021-03-08 17:20:07 +0900

    Create SY_B1034.cpp

---
+++ Baekjoon/2021-01_06/SY_B1034.cpp
@@ -0,0 +1,21 @@
+//b1034
+
+#include <iostream>
+using namespace std;
+int board[51][51];
+
+int main()
+{
+	int N, M; //
+	int k;
+
+	cin >> N >> M;
+	for(int row = 0; row < N; row++)
+	{
+		for(int col = 0; col < M; col++){
+			cin >> board[row][col];
+		}
+	}
+	
+	return 0;
+]
